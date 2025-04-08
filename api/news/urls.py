0e1b7b6dff2369fa4710773192cdcdeddefcf5bc
ChiragKumargithub <chirags2022003@gmail.com>
2025-04-08 11:40:43 +0530

    add username and logon picture on home page

--- api/news/urls.py
+++ api/news/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import news_api,home,register_view, login ,logout_view
+from .views import news_api,home,register_view, login ,logout_view,header_view
 
 
 urlpatterns = [
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/',register_view , name='register'),
     path('login/',login , name='login'),
     path('logout/',logout_view , name='logout'),
+    path('header/<int:id>/',header_view, name='header')
     
     
 ]
--- api/news/views.py
+++ api/news/views.py
@@ -6,7 +6,13 @@ import requests
 # Create your views here.
 def home(request):
              if 'login' in request.session:
-                return render(request,'home.html',{'loggin':True})
+                username = request.session['login']
+                
+                data= {
+                     'username': username
+                }
+                
+                return render(request,'home.html',{'loggin':True, 'data':data})
              else:
                 return render(request,'home.html') 
 
@@ -21,6 +27,15 @@ def register_view(request):
             return render(request, 'register.html')
      else:
            return render(request, 'register.html')
+     
+
+def header_view(request,id):
+     if 'login' in request.session:
+          username = register.objects.get(id = request.POST['username'])
+          return redirect('home',{'username': username})
+     else:
+          return render(request, 'register.html')
+               
            
 
 def login(request):
@@ -28,7 +43,7 @@ def login(request):
             try:
                 registered = register.objects.get(email = request.POST['email'])
                 if registered.password == request.POST['password']:
-                    request.session['login']= registered.email
+                    request.session['login']= registered.username
                     return redirect('home')
                 else:
                     return render(request, 'login.html')
